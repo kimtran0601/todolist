e40425defd1c2ac37fc76e29865823d529efc7cb
Kim Tran <kdt46@case.edu>
2023-04-10 01:18:35 -0400

    add home page and add dialouge

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,13 +1,31 @@
 <template>
-  <v-app-bar flat>
-    <v-app-bar-title>
-      <v-icon icon="mdi-circle-slice-4" />
-
-      Base Preset
+  <v-app-bar flat color="primary">
+    <v-app-bar-title align="center">
+      <v-icon icon="mdi-format-list-bulleted" />
+      Framework
     </v-app-bar-title>
+
+    <v-col cols="auto">
+      <v-btn
+        min-width="164"
+        rel="noopener noreferrer"
+        target="_blank"
+        variant="text"
+      >
+        Add
+        <v-icon
+          icon="mdi-plus"
+          size="large"
+          start
+        />
+        <template>
+          <AddTask />
+        </template>
+      </v-btn>
+    </v-col>
   </v-app-bar>
 </template>
 
 <script setup>
-  //
+  import AddTask from '@/layouts/default/AddTask.vue'
 </script>
--- src/layouts/default/Default.vue
+++ src/layouts/default/Default.vue
@@ -1,12 +1,13 @@
 <template>
   <v-app>
     <default-bar />
-
     <default-view />
+    <default-add />
   </v-app>
 </template>
 
 <script setup>
   import DefaultBar from './AppBar.vue'
   import DefaultView from './View.vue'
+  import DefaultAdd from './AddTask.vue'
 </script>
